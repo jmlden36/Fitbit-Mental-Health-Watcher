aae6e9917dcef879ef06c1d82bed1411f62ff90e
Matt Luker <jamesmattluker@gmail.com>
2022-02-26 10:32:17 -0700

    make a basic react app that makes a fetch call to my smartwatch

---
+++ mental-health-watcher/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../App.css';
+import WatchInfo from './WatchInfo';
+
+function App() {
+  return (
+    <React.Fragment>
+      < WatchInfo />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ mental-health-watcher/src/components/WatchInfo.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class Headlines extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      watchInfo: {}
+    };
+  }
+  makeApiCall = () => {
+    fetch(`https://api.fitbit.com/1/user/-/activities/heart/date/2022-02-24/2022-02-24.json`, {
+      method: "GET",
+      headers: {"authorization": `${process.env.REACT_APP_API_KEY}`}
+    })
+      .then(response => response.json())
+      .then(
+        (jsonifiedResponse) => {
+          console.log(jsonifiedResponse)
+          this.setState({
+            isLoaded: true,
+            watchInfo: jsonifiedResponse
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        });
+  }
+
+  componentDidMount() {
+    this.makeApiCall()
+  }
+  
+  render() {
+    const { error, isLoaded, watchInfo } = this.state;
+    let first = watchInfo['activities-heart-intraday'];
+    if (error) {
+      return <React.Fragment>Error: {error.message}</React.Fragment>;
+    } else if (!isLoaded) {
+      return <React.Fragment>Loading...</React.Fragment>;
+    } else {
+      return (
+        <React.Fragment>
+          <h1>WatchInfo</h1>
+          <ul>
+                <h3>{first.dataset[0].time} + {first.dataset[0].value}</h3>
+              
+          </ul>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+export default Headlines;
