101f54328c2e9bf618bfba95b35b107e848f50d9
Matt Luker <jamesmattluker@gmail.com>
2022-02-27 04:56:28 -0700

    add component files

--- mental-health-watcher/src/components/App.js
+++ mental-health-watcher/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './../App.css';
 import WatchInfo from './WatchInfo';
+import Heading from './Heading';
 
 function App() {
   return (
     <React.Fragment>
+      < Heading />
       < WatchInfo />
     </React.Fragment>
   );
---
+++ mental-health-watcher/src/components/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <React.Fragment>
+      <div className="header">
+        <h1 className="text-info">Mental Health Watcher</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Header;
