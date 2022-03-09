703a7c3a772a5a0af9206d1738dc97a317428e37
Matt Luker <jamesmattluker@gmail.com>
2022-03-09 10:37:40 -0700

    add stylin to header

--- mental-health-watcher/src/components/Heading.js
+++ mental-health-watcher/src/components/Heading.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
 function Header(){
   return (
     <React.Fragment>
       <div className="header">
+        <Link to="/" className="links">Home</Link>
+          <Link to="/signin" className="links">Sign In</Link>
         <h1>Mental Health Watcher</h1>
-        <h4>
-          <Link to="/">Home</Link>
-        </h4>
-        <h4>
-          <Link to="/signin">Sign In</Link>
-        </h4>
+          
       </div>
       
     </React.Fragment>
